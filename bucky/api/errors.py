c4207ce8422ce8eb5cdf4a68a9c459f67cc4721f
Fawaz Farid <fawwaz.ali@andela.com>
2017-02-04 23:21:51 +0300

    (feature) Custom API errors

---
+++ bucky/api/errors.py
@@ -0,0 +1,31 @@
+from flask import jsonify, make_response
+from ..exceptions import ValidationError
+
+
+def bad_request(message):
+    response = jsonify({'error': 'bad request', 'message': message})
+    response.status_code = 400
+    return response
+
+
+def unauthorized(message):
+    response = jsonify({'error': 'unauthorized', 'message': message})
+    response.status_code = 401
+    return response
+
+
+def forbidden(message):
+    response = jsonify({'error': 'forbidden', 'message': message})
+    response.status_code = 403
+    return response
+
+
+def register_routes(api):
+    @api.errorhandler(ValidationError)
+    def validation_error(e):
+        return bad_request(e.args[0])
+
+    @api.errorhandler(404)
+    def not_found(error):
+        print 'called'
+        return make_response(jsonify({'error': 'Resource not found.'}), 404)
